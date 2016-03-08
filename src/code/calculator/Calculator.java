c42023881c7a48340f00f68746eeee12e4086b6a
BelashArtem <artembelash1372@gmail.com>
2016-03-08 12:18:44 +0300

    Изменил 2-ичную СС на 27-ичную

--- src/code/calculator/Calculator.java
+++ src/code/calculator/Calculator.java
@@ -5,6 +5,6 @@ package code.calculator;
  */
 public class Calculator {
     public static void main(String[] args) {
-        CalcFrame frame = new CalcFrame(2, CalcFrame.MULTIPLICATION);
+        CalcFrame frame = new CalcFrame(27, CalcFrame.MULTIPLICATION);
     }
 }
